353b4c7e18ff99bec4eda261e08fd8292be90a8a
Dmitry Novichkov <dmitry1303@bk.ru>
2022-03-02 18:49:07 +0300

    Commit 3. (Changed)Added validate_time function, which check user input.

--- mainTest.py
+++ mainTest.py
@@ -3,6 +3,7 @@ from main import *
 
 
 class MyTestCase(unittest.TestCase):
+    # тестирование корректности ввода
     def test_validate_time(self):
         self.assertEqual(validate_time("19:00:00"), "ok")
         self.assertEqual(validate_time("24:24:24"), "Неверный формат часов, попробуйте снова")
