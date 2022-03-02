64345e2c6a483a945e19972a24152570ac9c4ca5
Dmitry Novichkov <dmitry1303@bk.ru>
2022-03-02 18:39:39 +0300

    Commit 2. Added validate_time function, which check user input.

---
+++ main.py
@@ -0,0 +1,16 @@
+from datetime import *
+from playsound import *
+
+# Проверка корректности ввода пользователем времени
+def validate_time(alarm_time):
+    if len(alarm_time) != 8:
+        return "Неверный формат, попробуйте снова"
+    else:
+        if int(alarm_time[0:2]) > 23:
+            return "Неверный формат часов, попробуйте снова"
+        elif int(alarm_time[3:5]) > 59:
+            return "Неверный формат минут, попробуйте снова"
+        elif int(alarm_time[6:8]) > 59:
+            return "Неверный формат секунд, попробуйте снова"
+        else:
+            return "ok"
--- mainTest.py
+++ mainTest.py
@@ -3,10 +3,9 @@ from main import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
+    def test_validate_time(self):
+        self.assertEqual(validate_time("19:00:00"), "ok")
+        self.assertEqual(validate_time("24:24:24"), "Неверный формат часов, попробуйте снова")
 
 
 
